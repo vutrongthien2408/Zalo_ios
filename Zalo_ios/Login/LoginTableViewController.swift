17adaef4fa3cd4b6f1a8e0b9fa399a1b6e6b7085
oVuTrongThien <vu.trong.thien@framgia.com>
2018-04-13 21:25:29 +0700

    login partent mvp

---
+++ Zalo_ios/Login/LoginTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginTableViewController.swift
+//  Zalo_ios
+//
+//  Created by Mac Pro on 4/8/18.
+//  Copyright © 2018 Mac Pro. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    @IBOutlet weak var tfUserName: HoshiTextField!
+    @IBOutlet weak var tfPassword: HoshiTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func getLoginData() -> Account? {
+        if let name = tfPassword.text, let pass = tfPassword.text,
+            !name.isEmpty, !pass.isEmpty {
+            let account = Account()
+            account.username = name
+            account.password = pass
+            return account
+        }
+        return nil
+    }
+    
+}
---
+++ Zalo_ios/Model/Account.swift
@@ -0,0 +1,21 @@
+//
+//  Account.swift
+//  Zalo_ios
+//
+//  Created by Mac Pro on 4/13/18.
+//  Copyright © 2018 Mac Pro. All rights reserved.
+//
+
+import Foundation
+
+class Account {
+    var username: String?
+    var password: String?
+    var address: String?
+    var phoneNumber: String?
+
+    init() {
+    }
+    
+    
+}
